f144b03b31b62e9126ca70cb75bcfa17e7c243c3
José Raúl Rentería Zavala <raul.renteria@sciodev.com>
2025-08-19 10:14:25 -0600

    footer corregido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink], // ✅ necesario
+  imports: [RouterOutlet, RouterLink, Footer], // ✅ necesario
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
